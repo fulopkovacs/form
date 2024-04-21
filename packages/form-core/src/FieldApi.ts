5c6877157fec7c7ae5551bd0042a468370193a07
Fülöp Kovács <kovacs.fulop@gmail.com>
2024-04-21 20:28:01 +0200

    Revert "Remove another console.log"
    This reverts commit 0bea13ce1be33e733296c71bda5b1fc96d4ca228.

--- packages/form-core/src/FieldApi.ts
+++ packages/form-core/src/FieldApi.ts
@@ -698,6 +698,13 @@ export class FieldApi<
           // clean up field errors
           const fieldErrorFromForm = things.fields[this.name]?.[errorMapKey]
           const fieldError = error || fieldErrorFromForm
+          console.info({
+            errorMapKey,
+            fieldError,
+            error,
+            fieldErrorFromForm,
+            field: field.name,
+          })
           field.setMeta((prev) => {
             return {
               ...prev,
